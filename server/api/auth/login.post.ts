c28e476a91c04803014ce42140d619b06dee9a55
khalidnaim18 <61711189+khalidkfy@users.noreply.github.com>
2026-01-26 12:18:55 +0200

    ;

--- server/api/auth/login.post.ts
+++ server/api/auth/login.post.ts
@@ -29,14 +29,14 @@ export default defineEventHandler(async (event) => {
         const config = useRuntimeConfig();
         const api_prefix = config.apiBase;
 
-        const profileData = await $fetch(`${api_prefix}/api/profile`, {
-          method: "GET",
-          headers: {
-            Accept: "application/json",
-            Authorization: res?.access_token ? `Bearer ${res?.access_token}` : "",
-          },
-        });
-        const addresses = profileData?.resource?.addresses || [];
+        // const profileData = await $fetch(`${api_prefix}/api/profile`, {
+        //   method: "GET",
+        //   headers: {
+        //     Accept: "application/json",
+        //     Authorization: res?.access_token ? `Bearer ${res?.access_token}` : "",
+        //   },
+        // });
+        // const addresses = profileData?.resource?.addresses || [];
         // TODO HANDLE MAIN ADDRESS API
 
         await setUserSession(event, {
@@ -46,12 +46,13 @@ export default defineEventHandler(async (event) => {
             mobile: res?.user.mobile,
             mobile_verified_at: res?.user.mobile_verified_at,
             name: res?.user.name,
-            addresses: addresses
+            // addresses: addresses
           },
           access_token: res?.access_token,
         });
+        console.log(res);
         return res;
-          console.log(res);
+          
       })
       .catch((err) => {
         console.log(err);
